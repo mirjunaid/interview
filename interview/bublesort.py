b2188e43ed469d3c0ecc88e27d4e3a2599df9f29
Junaid Ahmad Mir <79140912+mirjunaid@users.noreply.github.com>
2023-03-05 10:21:42 +0530

    Update bublesort.py

--- interview/bublesort.py
+++ interview/bublesort.py
@@ -1,7 +1,7 @@
 def bubleSort(items):
     change = 0
     for i in range(len(items)):
-        for j in range(len(items) - i- 1):
+        for j in range(len(items), i- 1):
             if items[j] > items[j + 1]:
                 items[j], items[j + 1] = items[j + 1], items[j]
                 change += change + 1
@@ -10,8 +10,6 @@ def bubleSort(items):
     print(items)
 
 print('Welcome to the buble sort algorithm')
-
-while True:
-    number = input('Enter the list items')
-    items = number.split 
-bubleSort(number)
+number = input('Enter the list items seperated by spaces: ')
+items = number.split 
+bubleSort(items)
